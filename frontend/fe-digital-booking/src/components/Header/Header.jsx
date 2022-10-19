5df07a4a797b403b37426ee920218386cca0ee8e
Felipe Monterrosa <felipe.monterrosa@hotmail.com>
2022-10-18 23:24:41 -0500

    feat(Header): adding mobile component

---
+++ frontend/fe-digital-booking/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.module.css";
+
+const Header = () => {
+  return (
+    <main>
+      <img src="assets/logo.svg" alt="DB Logo" />
+      <span class="material-icons">menu</span>
+    </main>
+  );
+};
+
+export default Header;
