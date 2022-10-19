cdaa26e1b2807c105034ad6f2d58b00ca150aa60
Felipe Monterrosa <felipe.monterrosa@hotmail.com>
2022-10-19 00:18:52 -0500

    fix(Header): changing classnames

--- frontend/fe-digital-booking/src/components/Header/Header.jsx
+++ frontend/fe-digital-booking/src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "./Header.module.css";
+import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <main>
+    <main className={styles["header-container"]}>
       <img src="assets/logo.svg" alt="DB Logo" />
       <span class="material-icons">menu</span>
     </main>
