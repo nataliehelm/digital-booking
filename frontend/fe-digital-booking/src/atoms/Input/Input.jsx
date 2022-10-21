f860c1302503054e30acab00a7ccb06cc388a4e0
Felipe Monterrosa <felipe.monterrosa@hotmail.com>
2022-10-20 19:02:59 -0500

    feat(Input): adding label styles

--- frontend/fe-digital-booking/src/atoms/Input/Input.jsx
+++ frontend/fe-digital-booking/src/atoms/Input/Input.jsx
@@ -1,10 +1,30 @@
-import "./Input.module.scss";
+import styles from "./Input.module.scss";
 import PropTypes from "prop-types";
+import { Text } from "../Text";
 
-const Input = ({ type, value, onChange }) => {
+const Input = ({
+  type,
+  value,
+  onChange,
+  hasError,
+  errorMessage,
+  label,
+  name,
+}) => {
   return (
     <div>
-      <input type={type} value={value} onChange={onChange} className="text-2" />
+      <label htmlFor={name}>
+        <Text variant="t2" classname={styles.label}>
+          {label}
+        </Text>
+      </label>
+      <input
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+        className="text-2"
+      />
     </div>
   );
 };
@@ -13,10 +33,15 @@ Input.propTypes = {
   type: PropTypes.string,
   value: PropTypes.string,
   onChange: PropTypes.func,
+  hasError: PropTypes.bool,
+  errorMessage: PropTypes.string,
+  label: PropTypes.string,
+  name: PropTypes.string.isRequired,
 };
 
 Input.defaultProps = {
   type: "text",
+  hasError: false,
 };
 
 export default Input;
