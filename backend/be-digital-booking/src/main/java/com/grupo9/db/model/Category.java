c8986df84a6faa3ca6de56ff69b7dce2f478ba68
Felipe Monterrosa <felipe.monterrosa@hotmail.com>
2022-10-22 02:54:12 -0500

    feat(Backend): adding createAt and updatedAt timestamps on categories crud

--- backend/be-digital-booking/src/main/java/com/grupo9/db/model/Category.java
+++ backend/be-digital-booking/src/main/java/com/grupo9/db/model/Category.java
@@ -1,6 +1,11 @@
 package com.grupo9.db.model;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 @Table(name = "category")
@@ -12,22 +17,34 @@ public class Category {
     private String description;
     private String image_url;
 
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+
+
     public Category() {
     }
 
-    public Category(Long id, String title, String description, String image_url) {
+    public Category(Long id, String title, String description, String image_url, Date createdAt, Date updatedAt) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.image_url = image_url;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -52,6 +69,22 @@ public class Category {
 
     public void setImage_url(String image_url) {
         this.image_url = image_url;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
     }
 
     @Override
