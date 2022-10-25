0090a78a655792093120ddd7eb5865ee22c7fa65
Natalia Garcia <nahiara@gmail.com>
2022-10-25 09:19:50 -0300

    Adding CategoryCard module. Fixing CategoryList

--- frontend/fe-digital-booking/src/components/CategoryList/CategoryList.jsx
+++ frontend/fe-digital-booking/src/components/CategoryList/CategoryList.jsx
@@ -1,25 +1,43 @@
 import React from "react";
 import styles from "./CategoryList.module.scss";
 import { Heading } from "../../atoms";
-import useFetch from "../../hook/useFetch";
+import useFetch from "../../hooks/useFetch";
+import CategoryCard from "../CategoryCard/CategoryCard";
 
 const CategoryList = () => {
-  const API_URL = `http://localhost:8081/categories`;
+  const API_URL = "http://localhost:8081/categories";
 
-  const { isLoading, errorMessage, apiData } = useFetch(API_URL);
-  console.log(apiData);
+  const { isLoading, errorMessage, data } = useFetch(API_URL);
+  console.log(data);
   if (isLoading)
     return (
-      <div className="App">
-        <p>Loading data...</p>
+      <div className={styles["category-list-container"]}>
+        <figure>
+          <img src="assets/loading-gif.gif" alt="Loading..."></img>
+        </figure>
       </div>
     );
 
   return (
-    <div className={styles["categoryList-container"]}>
-      <section>
+    <div className={styles["category-list-container"]}>
+      <div className={styles["list-header"]}>
         <Heading variant="h1">Buscar por tipo de alojamiento</Heading>
-      </section>
+      </div>
+      <div className={styles.list}>
+        <ul>
+          {data.slice(0, 4).map((categoria) => {
+            return (
+              <li key={categoria.id}>
+                <CategoryCard
+                  img={categoria.image_url}
+                  title={categoria.title}
+                  description={categoria.description}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 };
