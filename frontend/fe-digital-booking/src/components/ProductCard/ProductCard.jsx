7ff073cf25678661d222751e6eec572c2be3ee37
Felipe Monterrosa <felipe.monterrosa@hotmail.com>
2022-10-27 14:07:17 -0500

    fixing product card and list styles

--- frontend/fe-digital-booking/src/components/ProductCard/ProductCard.jsx
+++ frontend/fe-digital-booking/src/components/ProductCard/ProductCard.jsx
@@ -10,48 +10,44 @@ const ProductCard = ({
   location,
   description,
 }) => {
-  //const screen = window.screen.width;
   return (
     <div className={styles.card}>
-      <figure>
+      <figure className={styles['image-container']}>
+        <div className={styles.like}>
+          <i className="fa-solid fa-heart"></i>
+        </div>
         <img src={img} alt={title}></img>
       </figure>
-      <div className={styles.like}>
-        <i className="fa-solid fa-heart"></i>
-      </div>
       <div className={styles['card-description']}>
-        <div className={styles['product-name']}>
-          <div>
-            <Heading variant="h4">
-              {category}
-              <img src="assets/starts.png" alt="calificacion" />
-            </Heading>
-            <Heading variant="h1">{title}</Heading>
+        <div>
+          <div className={styles['product-name']}>
+            <div>
+              <Heading variant="h4">
+                {category}
+                <img src="assets/starts.png" alt="calificacion" />
+              </Heading>
+              <Heading variant="h1">{title}</Heading>
+            </div>
+            <div className={styles['score-container']}>
+              <span className={styles['product-score']}>{score}</span>
+              <p>Muy bueno</p>
+            </div>
           </div>
-          <div className={styles['score-container']}>
-            <span className={styles['product-score']}>{score}</span>
-            <p>Muy bueno</p>
+          <div className={styles.location}>
+            <i className="fa-solid fa-location-dot"></i>
+            <Text variant="t1">
+              {location} - <span> MOSTRAR EN EL MAPA</span>
+            </Text>
+          </div>
+          <div className={styles.services}>
+            <i className="fa-solid fa-wifi"></i>
+            <i className="fa-solid fa-person-swimming"></i>
           </div>
         </div>
-        <div className={styles.location}>
-          <i className="fa-solid fa-location-dot"></i>
-          <Text variant="t1">
-            {location} - <span> MOSTRAR EN EL MAPA</span>
-          </Text>
-        </div>
-        <div className={styles.services}>
-          <i className="fa-solid fa-wifi"></i>
-          <i className="fa-solid fa-person-swimming"></i>
-        </div>
-        <div className={styles.description}>
-          {/*           {screen > 768 ? (
-            <Heading variant="h4">
-              {description.substring(0, 65) + '...'}
-            </Heading>
-          ) : (
+        <div>
+          <div className={styles.description}>
             <Heading variant="h4">{description}</Heading>
-          )} */}
-          <Heading variant="h4">{description}</Heading>
+          </div>
           <Button onClick={() => {}} variant="b1">
             ver más
           </Button>
