7bee6bb92ec2f74233f5eb8ad677c2354d2c24fd
Felipe Monterrosa <felipe.monterrosa@hotmail.com>
2022-10-26 23:07:00 -0500

    feat: adding pathname conditional on header component

--- frontend/fe-digital-booking/src/components/AuthButtons/AuthButtons.jsx
+++ frontend/fe-digital-booking/src/components/AuthButtons/AuthButtons.jsx
@@ -1,21 +1,33 @@
-import React from "react";
-import { Button } from "../../atoms";
-import styles from "./AuthButtons.module.scss";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Button } from '../../atoms';
+import styles from './AuthButtons.module.scss';
+import { Link, useLocation } from 'react-router-dom';
 
 const AuthButtons = () => {
+  const { pathname } = useLocation();
   return (
     <section className={styles.container}>
-      <Link to="/signin">
-        <Button variant="b2" onClick={console.log}>
-          Crear cuenta
-        </Button>
-      </Link>
-      <Link to="/login">
-        <Button variant="b2" onClick={console.log}>
-          Iniciar sesión
-        </Button>
-      </Link>
+      {pathname.includes('login') ? (
+        <>
+          <div></div>
+          <Link to="/signin">
+            <Button variant="b2" onClick={console.log}>
+              Crear cuenta
+            </Button>
+          </Link>
+        </>
+      ) : (
+        <div></div>
+      )}
+      {pathname.includes('signin') && (
+        <>
+          <Link to="/login">
+            <Button variant="b2" onClick={console.log}>
+              Iniciar sesión
+            </Button>
+          </Link>
+        </>
+      )}
     </section>
   );
 };
