b596dda2e3dfa25071803a561135d1ed1ca2a4a3
Felipe Monterrosa <felipe.monterrosa@hotmail.com>
2022-10-26 20:16:39 -0500

    fix(AuthButtons): fixing spacing

--- frontend/fe-digital-booking/src/components/Header/Header.jsx
+++ frontend/fe-digital-booking/src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-import AuthButtons from "../AuthButtons";
-import { Drawer } from "../Drawer";
-import styles from "./Header.module.scss";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import AuthButtons from '../AuthButtons';
+import { Drawer } from '../Drawer';
+import styles from './Header.module.scss';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const [showDrawer, setShowDrawer] = useState(false);
-  const username = localStorage.getItem("username");
+  const username = localStorage.getItem('username');
 
   return (
     <>
@@ -14,15 +14,15 @@ const Header = () => {
         <Drawer username={username} setShowDrawer={setShowDrawer} />
       )}
       {!showDrawer && (
-        <main className={styles["header-container"]} id="header">
+        <main className={styles['header-container']} id="header">
           <Link to="/">
             <figure>
               <img src="assets/logo.svg" alt="DB Logo" />
-              <span className={styles["slogan"]}>Sentite como en tu hogar</span>
+              <span className={styles['slogan']}>Sentite como en tu hogar</span>
             </figure>
           </Link>
           <button
-            className={styles["menu-icon"]}
+            className={styles['menu-icon']}
             onClick={() => setShowDrawer(true)}
           >
             <span className="material-icons">
