7ceda6a5026449b14a7cb0faf934faa7e7513052
Felipe Monterrosa <felipe.monterrosa@hotmail.com>
2022-11-01 20:57:56 -0500

    feat(Map): adding component without unit test

---
+++ frontend/fe-digital-booking/src/pages/Product/components/Map/Map.js
@@ -0,0 +1,43 @@
+import { MapContainer, TileLayer, Marker, Tooltip } from 'react-leaflet';
+import Heading from '../../../../atoms/Heading/Heading';
+import './leaflet.scss';
+import styles from './Map.module.scss';
+import PropTypes from 'prop-types';
+
+const Map = ({ location }) => {
+  return (
+    <section>
+      <Heading variant="h1" classname={styles['map-title']}>
+        ¿Dónde vas a estar?
+      </Heading>
+      <div className={styles.divider}></div>
+      <Heading variant="h3" classname={styles['location']}>
+        {location}
+      </Heading>
+
+      <div className={styles['map-container']}>
+        <MapContainer
+          center={[-34.5828949, -58.4240502]}
+          zoom={20}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[-34.5828949, -58.4240502]}>
+            <Tooltip direction="right" offset={[0, 0]} opacity={1} permanent>
+              Hola 🧐
+            </Tooltip>
+          </Marker>
+        </MapContainer>
+      </div>
+    </section>
+  );
+};
+
+Map.propTypes = {
+  location: PropTypes.string.isRequired,
+};
+
+export default Map;
