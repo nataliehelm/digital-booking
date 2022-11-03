6b9697547d2470e1a3c410ac58dd391cc093b550
Natalia Garcia <nahiara@gmail.com>
2022-11-02 21:14:43 -0300

    feat(Subheader): adding Subheader component

---
+++ frontend/fe-digital-booking/src/atoms/Subheader/Subheader.jsx
@@ -0,0 +1,19 @@
+import { Heading } from '..';
+import Rank from '../Rank/Rank';
+import styles from './Subheader.module.scss';
+
+const Subheader = ({ title, subtitle, onBackClick }) => {
+  return (
+    <section className={styles['subheader-container']}>
+      <aside>
+        <Heading variant="h4">{title}</Heading>
+        <Heading variant="h1">{subtitle}</Heading>
+      </aside>
+      <figure onClick={onBackClick}>
+        <i className="fa-solid fa-chevron-left"></i>
+      </figure>
+    </section>
+  );
+};
+
+export default Subheader;
