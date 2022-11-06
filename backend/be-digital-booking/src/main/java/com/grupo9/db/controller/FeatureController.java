ca416f54998f69a7ea8b751ecb2046efe3d11f6a
Felipe Monterrosa <felipe.monterrosa@hunty.com>
2022-11-05 22:43:14 -0500

    feat(Backend): add feature CRUD

---
+++ backend/be-digital-booking/src/main/java/com/grupo9/db/controller/FeatureController.java
@@ -0,0 +1,46 @@
+package com.grupo9.db.controller;
+
+import com.grupo9.db.exceptions.BadRequestException;
+import com.grupo9.db.exceptions.ResourceNotFoundException;
+import com.grupo9.db.model.Feature;
+import com.grupo9.db.service.FeatureService;
+import com.grupo9.db.util.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/features")
+public class FeatureController {
+    @Autowired
+    private FeatureService service;
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<Feature>, Object>> findAll(){
+        return service.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<ApiResponse<Feature, Object>> findById(@PathVariable("id") Long id) throws ResourceNotFoundException  {
+        return service.findById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<Feature, Object>> save(@Valid @RequestBody Feature feature){
+        return service.save(feature);
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<ApiResponse<Feature, Object>> update(@PathVariable("id") Long id, @Valid @RequestBody Feature feature) throws ResourceNotFoundException, BadRequestException {
+        return service.update(id, feature);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<ApiResponse> deleteById(@PathVariable("id") Long id) throws ResourceNotFoundException, BadRequestException {
+        return service.deleteById(id);
+    }
+
+}
