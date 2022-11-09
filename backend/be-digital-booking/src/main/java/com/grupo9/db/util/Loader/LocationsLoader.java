d9cb337bd8b8e0f09d8921ad2731ad2aa1288751
Natalia Garcia <nahiara@gmail.com>
2022-11-08 21:07:43 -0300

    refactor(Location): refactoring Location model and loader

--- backend/be-digital-booking/src/main/java/com/grupo9/db/util/Loader/LocationsLoader.java
+++ backend/be-digital-booking/src/main/java/com/grupo9/db/util/Loader/LocationsLoader.java
@@ -15,30 +15,30 @@ public class LocationsLoader {
 
     public void Loader (){
 
-        Location location1 = new Location("Provincia de Misiones", "AR-N", "Misiones", "Provincia", "Misiones", "Argentina");
-        Location location2 = new Location("Provincia de San Luis", "AR-D", "San Luis", "Provincia", "San Luis", "Argentina");
-        Location location3 = new Location("Provincia de San Juan", "AR-J", "San Juan", "Provincia", "San Juan", "Argentina");
-        Location location4 = new Location("Provincia de Entre Ríos", "AR-E", "Entre Ríos", "Provincia", "Entre Ríos", "Argentina");
-        Location location5 = new Location("Provincia de Santa Cruz", "AR-Z", "Santa Cruz", "Provincia", "Santa Cruz", "Argentina");
-        Location location6 = new Location("Provincia de Río Negro", "AR-R", "Río Negro", "Provincia", "Río Negro", "Argentina");
-        Location location7 = new Location("Provincia del Chubut", "AR-U", "Chubut", "Provincia", "Chubut", "Argentina");
-        Location location8 = new Location("Provincia de Córdoba", "AR-X", "Córdoba", "Provincia", "Córdoba", "Argentina");
-        Location location9 = new Location("Provincia de Mendoza", "AR-M", "Mendoza", "Provincia", "Mendoza", "Argentina");
-        Location location10 = new Location("Provincia de La Rioja", "AR-F", "La Rioja", "Provincia", "La Rioja", "Argentina");
-        Location location11 = new Location("Provincia de Catamarca", "AR-K", "Catamarca", "Provincia", "Catamarca", "Argentina");
-        Location location12 = new Location("Provincia de La Pampa", "AR-L", "La Pampa", "Provincia", "La Pampa", "Argentina");
-        Location location13 = new Location("Provincia de Santiago del Estero", "AR-G", "Santiago del Estero", "Provincia", "Santiago del Estero", "Argentina");
-        Location location14 = new Location("Provincia de Corrientes", "AR-W", "Corrientes", "Provincia", "Corrientes", "Argentina");
-        Location location15 = new Location("Provincia de Santa Fe", "AR-S", "Santa Fe", "Provincia", "Santa Fe", "Argentina");
-        Location location16 = new Location("Provincia de Tucumán", "AR-T", "Tucumán", "Provincia", "Tucumán", "Argentina");
-        Location location17 = new Location("Provincia del Neuquén", "AR-Q", "Neuquén", "Provincia", "Neuquén", "Argentina");
-        Location location18 = new Location("Provincia de Salta", "AR-A", "Salta", "Provincia", "Salta", "Argentina");
-        Location location19 = new Location("Provincia del Chaco", "AR-H", "Chaco", "Provincia", "Chaco", "Argentina");
-        Location location20 = new Location("Provincia de Formosa", "AR-P", "Formosa", "Provincia", "Formosa", "Argentina");
-        Location location21 = new Location("Provincia de Jujuy", "AR-Y", "Jujuy", "Provincia", "Jujuy", "Argentina");
-        Location location22 = new Location("Ciudad Autónoma de Buenos Aires", "AR-C", "Ciudad Autónoma de Buenos Aires", "Ciudad Autónoma", "Ciudad Autónoma de Buenos Aires", "Argentina");
-        Location location23 = new Location("Provincia de Buenos Aires", "AR-B", "Buenos Aires", "Provincia", "Buenos Aires", "Argentina");
-        Location location24 = new Location("Provincia de Tierra del Fuego", "AR-V", "Tierra del Fuego", "Provincia", "Tierra del Fuego", "Argentina");
+        Location location1 = new Location("Provincia de Misiones", "Posadas", "Argentina");
+        Location location2 = new Location("Provincia de San Luis", "San Luis", "Argentina");
+        Location location3 = new Location("Provincia de San Juan", "San Juan", "Argentina");
+        Location location4 = new Location("Provincia de Entre Ríos", "Paraná", "Argentina");
+        Location location5 = new Location("Provincia de Santa Cruz", "Río Gallegos", "Argentina");
+        Location location6 = new Location("Provincia de Río Negro", "Viedma", "Argentina");
+        Location location7 = new Location("Provincia del Chubut", "Rawson", "Argentina");
+        Location location8 = new Location("Provincia de Córdoba", "Córdoba", "Argentina");
+        Location location9 = new Location("Provincia de Mendoza", "Mendoza", "Argentina");
+        Location location10 = new Location("Provincia de La Rioja", "La Rioja", "Argentina");
+        Location location11 = new Location("Provincia de Catamarca", "San Fernando del Valle de Catamarca", "Argentina");
+        Location location12 = new Location("Provincia de La Pampa", "Santa Rosa", "Argentina");
+        Location location13 = new Location("Provincia de Santiago del Estero",  "Santiago del Estero", "Argentina");
+        Location location14 = new Location("Provincia de Corrientes", "Corrientes", "Argentina");
+        Location location15 = new Location("Provincia de Santa Fe", "Santa Fe", "Argentina");
+        Location location16 = new Location("Provincia de Tucumán", "San Miguel de Tucumán", "Argentina");
+        Location location17 = new Location("Provincia del Neuquén", "Neuquén", "Argentina");
+        Location location18 = new Location("Provincia de Salta", "Salta", "Argentina");
+        Location location19 = new Location("Provincia del Chaco", "Resistencia", "Argentina");
+        Location location20 = new Location("Provincia de Formosa", "Formosa", "Argentina");
+        Location location21 = new Location("Provincia de Jujuy", "San Salvador de Jujuy", "Argentina");
+        Location location22 = new Location("Ciudad Autónoma de Buenos Aires", "Ciudad Autónoma de Buenos Aires", "Argentina");
+        Location location23 = new Location("Provincia de Buenos Aires", "La Plata", "Argentina");
+        Location location24 = new Location("Provincia de Tierra del Fuego", "Ushuaia", "Argentina");
 
         iLocationRepository.save(location1);
         iLocationRepository.save(location2);
