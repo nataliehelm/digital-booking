2a64d18732071eea7cf6083ca039bd3b12d3f939
Matias Menoni <rookiemat@gmail.com>
2022-12-03 22:45:22 -0300

    adding controller and service to upload files to s3 bucket

---
+++ backend/be-digital-booking/src/main/java/com/grupo9/db/controller/BucketController.java
@@ -0,0 +1,28 @@
+package com.grupo9.db.controller;
+
+import com.grupo9.db.service.AmazonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/storage/")
+public class BucketController {
+
+    private AmazonClient amazonClient;
+
+    @Autowired
+    BucketController(AmazonClient amazonClient) {
+        this.amazonClient = amazonClient;
+    }
+
+    @PostMapping("/uploadFile")
+    public String uploadFile(@RequestPart(value = "file") MultipartFile file) {
+        return this.amazonClient.uploadFile(file);
+    }
+
+    @DeleteMapping("/deleteFile")
+    public String deleteFile(@RequestPart(value = "url") String fileUrl) {
+        return this.amazonClient.deleteFileFromS3Bucket(fileUrl);
+    }
+}
