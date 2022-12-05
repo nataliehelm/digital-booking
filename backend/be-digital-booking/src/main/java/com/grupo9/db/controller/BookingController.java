ac898517008822bf146f7a9fbb265f209ef16950
romycavallaro <romycavallaro@gmail.com>
2022-12-05 19:09:11 -0300

    agrego valor default page

--- backend/be-digital-booking/src/main/java/com/grupo9/db/controller/BookingController.java
+++ backend/be-digital-booking/src/main/java/com/grupo9/db/controller/BookingController.java
@@ -16,6 +16,7 @@ import com.grupo9.db.util.ResponsesBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -46,7 +47,7 @@ public class BookingController {
     }
 
     @GetMapping(path = "/user/{id}")
-    public ResponseEntity<ApiResponse<Page<Booking>, Object>> findAllBookingsByUserId(@PathVariable("id") Long id, Pageable pageable) throws ResourceNotFoundException {
+    public ResponseEntity<ApiResponse<Page<Booking>, Object>> findAllBookingsByUserId(@PathVariable("id") Long id, @PageableDefault(size = 8) Pageable pageable) throws ResourceNotFoundException {
         Page<Booking> bookings = bookingService.findAllBookingsByUserId(id, pageable);
         return responsesBuilder.buildResponse(HttpStatus.OK.value(),"Get Booking List by user Id successfully",bookings, null);
     }
