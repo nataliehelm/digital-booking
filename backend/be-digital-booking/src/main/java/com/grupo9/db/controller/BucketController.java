d995cab7404ae939dd2bd1a63d540578c9a14dbe
Matias Menoni <rookiemat@gmail.com>
2022-12-05 22:12:16 -0300

    fixing aws responses

--- backend/be-digital-booking/src/main/java/com/grupo9/db/controller/BucketController.java
+++ backend/be-digital-booking/src/main/java/com/grupo9/db/controller/BucketController.java
@@ -1,9 +1,17 @@
 package com.grupo9.db.controller;
 
+import com.grupo9.db.model.Category;
 import com.grupo9.db.service.AmazonClient;
+import com.grupo9.db.util.ApiResponse;
+import com.grupo9.db.util.ResponsesBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/storage/")
@@ -12,17 +20,24 @@ public class BucketController {
     private AmazonClient amazonClient;
 
     @Autowired
+    private ResponsesBuilder responsesBuilder;
+
+    @Autowired
     BucketController(AmazonClient amazonClient) {
         this.amazonClient = amazonClient;
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/uploadFile")
-    public String uploadFile(@RequestPart(value = "file") MultipartFile file) {
-        return this.amazonClient.uploadFile(file);
+    public ResponseEntity<ApiResponse<String, Object>> uploadFile(@RequestPart(value = "file") MultipartFile file) {
+        String response = this.amazonClient.uploadFile(file);
+        return responsesBuilder.buildResponse(HttpStatus.CREATED.value(),"Image uploaded successfully",response, null);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/deleteFile")
-    public String deleteFile(@RequestPart(value = "url") String fileUrl) {
-        return this.amazonClient.deleteFileFromS3Bucket(fileUrl);
+    public ResponseEntity<ApiResponse<String, Object>>  deleteFile(@RequestPart(value = "url") String fileUrl) {
+        String response = this.amazonClient.deleteFileFromS3Bucket(fileUrl);
+        return responsesBuilder.buildResponse(HttpStatus.OK.value(),"Image deleted successfully",response, null);
     }
 }
